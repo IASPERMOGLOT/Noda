6e2f34b0d6d64ab1d26089f2db05eeff435df6e9
IASPERMOGLOT <bulatooov@gmail.com>
2025-10-03 18:14:09 +0300

    Initial Commit

---
+++ Noda/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Noda
+//
+//  Created by Александр on 03.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Noda/NodaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NodaApp.swift
+//  Noda
+//
+//  Created by Александр on 03.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct NodaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
