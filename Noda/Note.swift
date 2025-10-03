7a9b3010906ca27dc87b40d539e3e5cac1283436
IASPERMOGLOT <bulatooov@gmail.com>
2025-10-03 20:22:22 +0300

    Note + NoteRow

---
+++ Noda/Note.swift
@@ -0,0 +1,8 @@
+//
+//  Note.swift
+//  Noda
+//
+//  Created by Александр on 03.10.2025.
+//
+
+import Foundation
---
+++ Noda/NoteRow.swift
@@ -0,0 +1,18 @@
+//
+//  NoteRow.swift
+//  Noda
+//
+//  Created by Александр on 03.10.2025.
+//
+
+import SwiftUI
+
+struct NoteRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteRow()
+}
